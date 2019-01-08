173e6810328c21e35cff964b6025112ce21e919a
Martin Kousal <mkousal1@gmail.com>
2019-01-08 21:19:19 +0100

    Add photos to gallery

--- generate_list.py
+++ generate_list.py
@@ -1,10 +1,10 @@
 import os
 
-arr = os.listdir('C:\\Users\\Martin\\Documents\\Github\\robotickytabor.cz\\img\\2018')
+arr = os.listdir('C:\\Users\\Martin\\Documents\\Github\\robotickytabor.cz\\img\\2017')
 
 f = open('C:\\Users\\Martin\\Documents\\Github\\robotickytabor.cz\\generated_list.txt', "w+")
 
-for i in range (72):
-  f.write("<li><a href=\"img/2018/%s\" data-imagelightbox=\"g\"><img src=\"thumb/2018/%s\" alt=\" \"/></a></li>\n" % (arr [i], arr [i]))
+for i in range (97):
+  f.write("<li><a href=\"img/2017/%s\" data-imagelightbox=\"g\"><img src=\"thumb/2017/%s\" alt=\" \"/></a></li>\n" % (arr [i], arr [i]))
 
 f.close()
