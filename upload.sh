59739eb137711bf0147d5d0d3d828f232108d0ae
Jan Mrázek <email@honzamrazek.cz>
2019-01-09 23:45:18 +0100

    Try it with HTTPS

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Uploading $1"
-curl -sS -F "file=@$1" -F "token=$HOST_SECRET" -F "path=$1" $HOST_UPLOAD
+curl -sS -k -F "file=@$1" -F "token=$HOST_SECRET" -F "path=$1" $HOST_UPLOAD
